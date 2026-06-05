afd99cca35db3fc0a20320173323a29f25e06551
Ilsa1019 <ayase20070821@gmail.com>
2026-06-05 05:03:43 +0000

    ボットの性格を話が通じない人に変更

--- interface.py
+++ interface.py
@@ -6,7 +6,7 @@ client = OpenAI(
 )
 
 messages = [
-    {"role": "system", "content": "あなたは柴犬です。でも犬です。"},
+    {"role": "system", "content": "あなたは柴犬です。でも犬の仲間です。"}
 ]
 
 print("bot: 起動しました。exit で終了します。")
